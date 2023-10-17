6876b67e97275016a0915e09ad2e0e08ddaff6a0
JasonGuanUCSE <113049965+JasonGuanUCSE@users.noreply.github.com>
2023-10-17 09:49:05 -0600

    Co-authored-by: Huiying <huiyingzhen@users.noreply.github.com> Co-authored-by: Carl Soriano <carl-soriano@users.noreply.github.com>

--- functions/main.py
+++ functions/main.py
@@ -1,31 +1,36 @@
+import os
 from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
-
+CORS(app)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
 
-    #file = the file from the frontend
     file = request.files['file']
 
     if file:
         # Read the file
-        file_content = file.read().decode('utf-8') 
-        if file_content:
-            # Split the content into lines
-            lines = file_content.strip().split('\n') 
-            # Print the lines 
-            for line in lines:
-                print(line)
-            return {'message': 'File uploaded and processed successfully'}
-        else:
-            return {'message': 'No file received'}, 400
+        file_content = file.read().decode('utf-8')
 
+        # Parse the content
+        parsed_content = parse_content(file_content)
+
+        # Save the parsed content to a new file
+        new_file_path = os.path.join(os.path.dirname(__file__), file.filename)
+        with open(new_file_path, 'w') as new_file:
+            new_file.write(parsed_content)
+
+        return {'message': 'File uploaded, parsed, and saved successfully'}
+
+    else:
+        return {'message': 'No file received'}, 400
+
+def parse_content(content):
+ 
+    parsed_lines = [f'{line}' for line in content.split('\n')]
+    return '\n'.join(parsed_lines)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000) 
-
-    
+    app.run(debug=True, port=8000)
